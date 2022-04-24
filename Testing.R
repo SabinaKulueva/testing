cc6181dc5c176873978a9095bcf8aa780964be66
SabinaKulueva <lady.culuewa@yandex.ru>
2022-04-24 23:17:46 +0200

    Personalized Token is used during pushing the commit

--- Testing.R
+++ Testing.R
@@ -1,3 +1,5 @@
 print ("This file was created within RStudio")
 
 print ("And now it lives on GitHub")
+
+print ("I had to create personalized token and use that token instead of password during pushing the commit")
